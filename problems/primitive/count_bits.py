0ad6a35b066c04e6551a08be8883a37f4fc34211
Darian W <darianjupiter@gmail.com>
2020-11-27 19:59:11 -0800

    added count_bits solution

---
+++ problems/primitive/count_bits.py
@@ -0,0 +1,19 @@
+# Count the number of bits set to 1
+
+# O(n) where n is number of bits to represent x
+def count_bits(x):
+    num_bits = 0
+    while x:
+        num_bits += x & 1
+        x >>= 1
+    return num_bits
+
+def main():
+    print(count_bits(0b10110))
+    print(count_bits(0x12A))
+        #0001 0010 1010
+    print(count_bits(100))
+        #100 = 0b1100100
+
+if __name__ == '__main__':
+    main()
