a10878d2897ec2bf5ae5c967ac4e400167c5c569
Darian W <darianjupiter@gmail.com>
2020-11-27 20:18:34 -0800

    added dutch_national_flag solution and test cases

---
+++ problems/arrays/dutch_national_flag.py
@@ -0,0 +1,107 @@
+import functools
+from typing import List
+
+from test_framework import generic_test
+from test_framework.test_failure import TestFailure
+from test_framework.test_utils import enable_executor_hook
+
+RED, WHITE, BLUE = range(3)
+
+# Space: O(1), Time: O(n)
+# Single pass instead of 2 pass
+def dutch_flag_partitionD(A: List[int], pivot_index: int) -> None:
+    pivot = A[pivot_index] 
+    smaller, equal, larger = 0, 0, len(A)-1
+    while equal < larger: # while unclassified element A[equal:larger]
+        if A[equal] < pivot:
+            A[smaller], A[equal] = A[equal], A[smaller]
+            smaller, equal = smaller+1, equal+1
+        elif A[equal] > pivot:
+            A[larger], A[equal] = A[equal], A[larger]
+            larger -= 1 # don't inc equal
+        else: # A[equal] == pivot
+            equal += 1
+
+# Space: O(1), Time: O(n)
+# Similar to B but instead of search for swap, keep track of subarrays
+def dutch_flag_partitionC(A: List[int], pivot_index: int) -> None:
+    pivot = A[pivot_index]
+    smaller = 0
+    for i in range(len(A)):
+        if A[i] < pivot:
+            A[i], A[smaller] = A[smaller], A[i]
+            smaller += 1
+
+    larger = len(A)-1
+    for i in reversed(range(len(A))):
+        if A[i] < pivot:
+            break
+        elif A[i] > pivot:
+            A[i], A[larger] = A[larger], A[i]
+            larger -= 1
+
+# Space: O(1), Time: O(n^2)
+# Last test took a long time.
+def dutch_flag_partitionB(A: List[int], pivot_index: int) -> None:
+    pivot = A[pivot_index]
+    for i in range(len(A)):
+        # Find/Swap with smaller
+        for j in range(i+1,len(A)): # doesn't include i
+            if A[j] < pivot:
+                A[i], A[j] = A[j], A[i]
+                break
+
+    for i in reversed(range(len(A))):
+        if A[i] < pivot:
+            break # Stop when reach less than pivot since already correct
+        # Find/Swap with larger
+        for j in reversed(range(i)): # doesn't include i (i-1 to 0)
+            if A[j] > pivot:
+                A[i], A[j] = A[j], A[i]
+                break
+
+# Space: extra O(n) , Time: O(n)
+def dutch_flag_partitionA(A: List[int], pivot_index: int) -> None:
+    less, equal, greater = [], [], []
+    pivot = A[pivot_index]
+    for item in A:
+        if item < pivot:
+            less.append(item)
+        elif item > pivot:
+            greater.append(item)
+        else:
+            equal.append(item)
+        
+    A = less + equal + greater
+ 
+@enable_executor_hook
+def dutch_flag_partition_wrapper(executor, A, pivot_idx):
+    count = [0, 0, 0]
+    for x in A:
+        count[x] += 1
+    pivot = A[pivot_idx]
+
+    executor.run(functools.partial(dutch_flag_partition, pivot_idx, A))
+
+    i = 0
+    while i < len(A) and A[i] < pivot:
+        count[A[i]] -= 1
+        i += 1
+    while i < len(A) and A[i] == pivot:
+        count[A[i]] -= 1
+        i += 1
+    while i < len(A) and A[i] > pivot:
+        count[A[i]] -= 1
+        i += 1
+
+    if i != len(A):
+        raise TestFailure('Not partitioned after {}th element'.format(i))
+    elif any(count):
+        raise TestFailure('Some elements are missing from original array')
+
+
+if __name__ == '__main__':
+    exit(
+        generic_test.generic_test_main('dutch_national_flag.py',
+                                       'dutch_national_flag.tsv',
+                                       dutch_flag_partitionD))
