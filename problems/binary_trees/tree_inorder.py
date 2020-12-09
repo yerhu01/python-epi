27303a3ee18e89c7f4d6ee60de32b6f8bc185d35
Darian W <darianjupiter@gmail.com>
2020-12-09 14:59:03 -0800

    recursive inorder traversal solution

---
+++ problems/binary_trees/tree_inorder.py
@@ -0,0 +1,26 @@
+from typing import List
+
+from binary_tree_node import BinaryTreeNode
+from test_framework import generic_test
+
+# Iterative Implementation
+# TODO
+def inorder_traversal(tree: BinaryTreeNode) -> List[int]:
+    pass
+
+# Recursive Implementation
+# Space: O(n) Time: O(h)  where h is n if skewed tree, logn if complete
+def inorder_traversalr(tree: BinaryTreeNode) -> List[int]:
+    def traverse(root):
+        if root:
+            traverse(root.left)
+            result.append(root.data)
+            traverse(root.right)
+    result = []
+    traverse(tree)
+    return result
+        
+if __name__ == '__main__':
+    exit(
+        generic_test.generic_test_main('tree_inorder.py', 'tree_inorder.tsv',
+                                       inorder_traversalr))
