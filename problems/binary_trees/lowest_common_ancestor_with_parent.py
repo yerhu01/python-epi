f48bd2aac8fd8bc4ae7d4b33b29ca4facd7b07aa
Darian W <darianjupiter@gmail.com>
2020-12-15 19:00:04 -0800

    LCA when nodes have parent pointers

---
+++ problems/binary_trees/lowest_common_ancestor_with_parent.py
@@ -0,0 +1,50 @@
+import functools
+from typing import Optional
+
+from binary_tree_with_parent_prototype import BinaryTreeNode
+from test_framework import generic_test
+from test_framework.binary_tree_utils import must_find_node
+from test_framework.test_failure import TestFailure
+from test_framework.test_utils import enable_executor_hook
+
+# Time: O(h) Space: O(1)
+def lca(node0: BinaryTreeNode,
+        node1: BinaryTreeNode) -> Optional[BinaryTreeNode]:
+    def get_depth(node):
+        depth = 0
+        while node:
+            depth += 1
+            node = node.parent
+        return depth
+
+    depth0, depth1 = get_depth(node0), get_depth(node1)
+    if depth1 > depth0:
+        depth0, depth1 = depth1, depth0
+        node0, node1 = node1, node0  
+
+    # Ascend to match depth
+    while depth0 > depth1:
+        node0 = node0.parent
+        depth0 -= 1 
+
+    # Ascend both until LCA
+    while node0 is not node1:
+        node0, node1 = node0.parent, node1.parent
+    return node0
+
+@enable_executor_hook
+def lca_wrapper(executor, tree, node0, node1):
+    result = executor.run(
+        functools.partial(lca, must_find_node(tree, node0),
+                          must_find_node(tree, node1)))
+
+    if result is None:
+        raise TestFailure('Result can\'t be None')
+    return result.data
+
+
+if __name__ == '__main__':
+    exit(
+        generic_test.generic_test_main('lowest_common_ancestor_with_parent.py',
+                                       'lowest_common_ancestor.tsv',
+                                       lca_wrapper))
